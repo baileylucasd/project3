c2ffdbe8cff65b3a603e0b40be84b75c2fa9708e
Luke Bailey <baileylucasd@gmail.com>
2021-03-11 09:17:27 -0500

    test PS C:\Users\yermo\OneDrive\Desktop\backpack\cis371\assignment3>

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import axios, {AxiosResponse} from "axios";
+
+const catURL = "https://api.thecatapi.com/v1/images/search";
+axios
+    .get(catURL, {
+        params: {
+            breed_ids: "siam",
+        },
+    })
+    .then((r: AxiosResponse) => r.data)
+    .then((breeds: any[]) => {
+        console.log(breeds);
+    });
+
+    let theInput: HTMLInputElement | null;
+    let thebutton: HTMLButtonElement | null;
+    theInput = document.querySelector("#userInput > input[type=text]");
+    thebutton = document.querySelector("#userInput > button");
+    
+    thebutton?.addEventListener("click", () => {
+      const inputLen = theInput?.value.length ?? 0;
+      if (inputLen > 0)  
+        console.log("You entered", theInput?.value);
+      else
+        console.log("Please enter some text");
+    });
